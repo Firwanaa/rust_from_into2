3a1455c0a02ec816256e5a23e5824a4769d014c9
firwanaa <firwanaa@sheridancollege.ca>
2021-11-29 11:00:47 -0500

    Demo from_into 2

---
+++ target/debug/deps/from_into_demo2-801f6541475454ea.d
@@ -0,0 +1,5 @@
+/run/media/qq/qq2/Dev/rust_projects/demos/from_into_demo2/target/debug/deps/from_into_demo2-801f6541475454ea.rmeta: src/main.rs
+
+/run/media/qq/qq2/Dev/rust_projects/demos/from_into_demo2/target/debug/deps/from_into_demo2-801f6541475454ea.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/from_into_demo2-b8544a7ce77091ed.d
@@ -0,0 +1,5 @@
+/run/media/qq/qq2/Dev/rust_projects/demos/from_into_demo2/target/debug/deps/from_into_demo2-b8544a7ce77091ed.rmeta: src/main.rs
+
+/run/media/qq/qq2/Dev/rust_projects/demos/from_into_demo2/target/debug/deps/from_into_demo2-b8544a7ce77091ed.d: src/main.rs
+
+src/main.rs:
